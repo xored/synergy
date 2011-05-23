9127ee6c4ca7969ff0c6a139f7b91137295402f4
Victor Bilyk <victorbilyk@gmail.com>
2011-05-23 21:47:58 +0700

    all tests passed

---
+++ emfjson/com.xored.emfjson-sherlock-test/src/com/xored/emfjson/EmfJsonSherlockTest.java
@@ -0,0 +1,87 @@
+package com.xored.emfjson;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import com.xored.sherlock.core.model.sherlock.EclipseInfo;
+import com.xored.sherlock.core.model.sherlock.EclipsePlugin;
+import com.xored.sherlock.core.model.sherlock.SherlockFactory;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonArray;
+
+import org.junit.Test;
+
+public class EmfJsonSherlockTest {
+
+  @Test
+  public void testPluginSerialization() throws Exception {
+    EclipsePlugin plugin = samplePlugin();
+
+    Emf2Json emf2json = new Emf2Json();
+    JsonObject object = emf2json.serialize(plugin);
+
+    assertEquals(object.get("name"), new JsonPrimitive("plugin-name"));
+    assertEquals(object.get("provider"), new JsonPrimitive("plugin-provider"));
+    assertEquals(object.get("id"), new JsonPrimitive("plugin-id"));
+    assertEquals(object.get("version"), new JsonPrimitive("plugin-version"));
+  }
+
+  @Test
+  public void testEclipseInfoSerialization() throws Exception {
+    final EclipseInfo info = sampleInfo();
+
+    Emf2Json emf2json = new Emf2Json();
+    JsonObject object = emf2json.serialize(info);
+    
+    JsonArray plugins = object.getAsJsonArray("plugins");
+    assertEquals(10, plugins.size());
+    JsonObject fourth = (JsonObject)plugins.get(3);
+    
+    assertEquals(fourth.get("name"), new JsonPrimitive("plugin-name"));
+    assertEquals(fourth.get("provider"), new JsonPrimitive("plugin-provider"));
+    assertEquals(fourth.get("id"), new JsonPrimitive("plugin-id"));
+    assertEquals(fourth.get("version"), new JsonPrimitive("plugin-version"));
+  }
+
+  @Test
+  public void testEclipseInfoInMapSerialization() throws Exception {
+    final EclipseInfo info = sampleInfo();
+    Emf2Json emf2json = new Emf2Json();
+
+    JsonObject json = new JsonObject();
+    json.add("info", emf2json.serialize(info));
+
+    assertTrue(json.toString().contains("plugin-version"));
+  }
+
+  private EclipseInfo sampleInfo() {
+    final EclipseInfo info = SherlockFactory.eINSTANCE.createEclipseInfo();
+    info.setBuildId("build-id");
+    info.setWorkspaceLocation("workspace-location");
+    info.setUptime(10000);
+    info.setProductId("product-id");
+    info.setApplicationId("application-id");
+    for (int i=0;i<10;i++) {
+      info.getPlugins().add(samplePlugin());
+    }
+    return info;
+  }
+
+  private EclipsePlugin samplePlugin() {
+    final EclipsePlugin plugin = SherlockFactory.eINSTANCE.createEclipsePlugin();
+    plugin.setName("plugin-name");
+    plugin.setProvider("plugin-provider");
+    plugin.setId("plugin-id");
+    plugin.setVersion("plugin-version");
+    return plugin;
+  }
+
+}
+
--- x5agent/x5agent-plugin/src/internal/X5Listener.scala
+++ x5agent/x5agent-plugin/src/internal/X5Listener.scala
@@ -5,6 +5,8 @@ import com.xored.sherlock.core.info._
 import com.xored.emfjson._
 import java.util.Date
 import com.google.gson.Gson
+import com.google.gson.JsonObject
+import com.google.gson.JsonPrimitive
 import collection.JavaConversions
 
 import org.eclipse.core.runtime._
@@ -39,14 +41,14 @@ trait X5ListenerComponentImpl extends X5ListenerComponent {
       val javaInfo = emf2json.serialize(Info.getJava())
       val eclipseInfo = emf2json.serialize(Info.getEclipse())
 
-      val report = Map(
-        "eclipseStatus"->eclipseStatus
-        ,"systemInfo"->systemInfo
-        ,"javaInfo"->javaInfo
-        ,"eclipseInfo"->eclipseInfo
-        ,"pluginId"->pluginId
-      )
-      val jsonReport = gson toJson ( JavaConversions.asJavaMap( report ) )
+      val report = new JsonObject
+      report.add("eclipseStatus", eclipseStatus)
+      report.add("systemInfo", systemInfo)
+      report.add("javaInfo", javaInfo)
+      report.add("eclipseInfo", eclipseInfo)
+      report.add("pluginId", new JsonPrimitive(pluginId))
+
+      val jsonReport = report.toString
       val reportObj = Report(createdAt = new Date(), content = jsonReport)
       x5storage.save(reportObj)
       x5dispatcher.notifyGotSomethingToSubmit
