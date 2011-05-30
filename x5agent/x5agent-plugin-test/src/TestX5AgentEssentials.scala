dfbc314596fb8718ca1220d004b021f486fa5b7f
Victor Bilyk <victorbilyk@gmail.com>
2011-05-30 18:07:21 +0700

    presumably fixes bamboo builds

--- x5agent/x5agent-plugin-test/src/TestX5AgentEssentials.scala
+++ x5agent/x5agent-plugin-test/src/TestX5AgentEssentials.scala
@@ -5,7 +5,7 @@ import java.io.File
 
 object TestX5AgentEssentials {
   val uuid = UUID.randomUUID()
-  val userHome = System.getProperty("java.io.tmpdir")+"home"+uuid+"/"
+  val userHome = System.getProperty("java.io.tmpdir")+"/home"+uuid+"/"
 
   new File(userHome).mkdirs()
 } 
@@ -17,7 +17,7 @@ trait TestX5AgentEssentials extends X5AgentEssentials {
   override protected def logError(msg:String) = println(msg)
   override protected def logError(msg:String, t:Throwable) = println(msg+" "+t)
 
-  override protected def x5dirName:String = System.getProperty("java.io.tmpdir")
+  override protected def x5dirName:String = System.getProperty("java.io.tmpdir")+"/"
   override protected def userName:String = "x5user-"+TestX5AgentEssentials.uuid
   override protected def userHome:String = TestX5AgentEssentials.userHome 
 
