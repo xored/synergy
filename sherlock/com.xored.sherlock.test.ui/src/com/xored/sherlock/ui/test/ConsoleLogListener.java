1a9dbc7808b15204a41130074874ba36e184520e
Alena Repina <alenaRepina@gmail.com>
2011-08-04 20:10:52 +0700

    Update tests

--- sherlock/com.xored.sherlock.test.ui/src/com/xored/sherlock/ui/test/ConsoleLogListener.java
+++ sherlock/com.xored.sherlock.test.ui/src/com/xored/sherlock/ui/test/ConsoleLogListener.java
@@ -1,5 +1,7 @@
 package com.xored.sherlock.ui.test;
 
+import java.util.Map.Entry;
+
 import org.eclipse.core.runtime.ILogListener;
 import org.eclipse.core.runtime.IStatus;
 
@@ -10,9 +12,7 @@ import com.xored.sherlock.core.model.sherlock.EclipseInfo;
 import com.xored.sherlock.core.model.sherlock.EclipsePlugin;
 import com.xored.sherlock.core.model.sherlock.EclipseStatus;
 import com.xored.sherlock.core.model.sherlock.JavaInfo;
-import com.xored.sherlock.core.model.sherlock.JavaProperty;
 import com.xored.sherlock.core.model.sherlock.SystemInfo;
-import com.xored.sherlock.core.model.sherlock.SystemVariable;
 
 public class ConsoleLogListener implements ILogListener {
 	@Override
@@ -32,8 +32,8 @@ public class ConsoleLogListener implements ILogListener {
 		System.out.println("OS version: " + systemInfo.getOsVersion());
 		System.out.println("Username: " + systemInfo.getUsername());
 		System.out.println("Variables:");
-		for (SystemVariable var : systemInfo.getVariables()) {
-			System.out.println("\t" + var.getName() + "=" + var.getValue());
+		for (Entry<String, String> var : systemInfo.getVariables().entrySet()) {
+			System.out.println("\t" + var.getKey() + "=" + var.getValue());
 		}
 
 		System.out.println("== Java ==");
@@ -43,8 +43,8 @@ public class ConsoleLogListener implements ILogListener {
 		System.out.println("Max memory: " + javaInfo.getMaxMemory());
 		System.out.println("Total memory: " + javaInfo.getTotalMemory());
 		System.out.println("Properties:");
-		for (JavaProperty prop : javaInfo.getProperties()) {
-			System.out.println("\t" + prop.getName() + "=" + prop.getValue());
+		for (Entry<String, String> prop : javaInfo.getProperties().entrySet()) {
+			System.out.println("\t" + prop.getKey() + "=" + prop.getValue());
 		}
 
 		System.out.println("== Eclipse ==");
@@ -55,11 +55,13 @@ public class ConsoleLogListener implements ILogListener {
 		System.out.println("Uptime: " + eclipseInfo.getUptime());
 		System.out.println("Plugins:");
 		for (EclipsePlugin plugin : eclipseInfo.getPlugins()) {
-			System.out.println("\t" + plugin.getId() + ": " + plugin.getVersion());
+			System.out.println("\t" + plugin.getId() + ": "
+					+ plugin.getVersion());
 		}
 		System.out.println("Features:");
 		for (EclipseFeature feature : eclipseInfo.getFeatures()) {
-			System.out.println("\t" + feature.getId() + ": " + feature.getVersion());
+			System.out.println("\t" + feature.getId() + ": "
+					+ feature.getVersion());
 		}
 
 		System.out.println("<<<<=========================");
