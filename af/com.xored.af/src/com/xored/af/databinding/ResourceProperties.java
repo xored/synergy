00c241ec401ff853cb2722cfdeedb1e710650d3c
Igor Zapletnev <igor.zapletnev@gmail.com>
2013-08-26 08:48:14 +0700

    Revert commit a6122fd6e47c13fa610e424b4f7d9dd2aa4ffd3d

--- af/com.xored.af/src/com/xored/af/databinding/ResourceProperties.java
+++ af/com.xored.af/src/com/xored/af/databinding/ResourceProperties.java
@@ -11,164 +11,160 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.Resource.Diagnostic;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 
-@SuppressWarnings("rawtypes")
 public class ResourceProperties {
-	private static class Errors extends NotifierListProperty {
-		public Errors() {
-			super(Resource.RESOURCE__ERRORS, Diagnostic.class);
-		}
+    private static class Errors extends NotifierListProperty {
+        public Errors() {
+            super(Resource.RESOURCE__ERRORS, Diagnostic.class);
+        }
 
-		@SuppressWarnings("unchecked")
-		@Override
-		protected void doSetList(Object source, List list, ListDiff diff) {
-			final Resource resource = (Resource) source;
-			resource.getErrors().clear();
-			resource.getErrors().addAll(list);
-		}
+        @Override
+        protected void doSetList(Object source, List list, ListDiff diff) {
+            final Resource resource = (Resource) source;
+            resource.getErrors().clear();
+            resource.getErrors().addAll(list);
+        }
 
-		@Override
-		protected List doGetList(Object source) {
-			final Resource resource = (Resource) source;
-			return resource.getErrors();
-		}
-	}
+        @Override
+        protected List doGetList(Object source) {
+            final Resource resource = (Resource) source;
+            return resource.getErrors();
+        }
+    }
 
-	private static class Warnings extends NotifierListProperty {
-		public Warnings() {
-			super(Resource.RESOURCE__WARNINGS, Diagnostic.class);
-		}
+    private static class Warnings extends NotifierListProperty {
+        public Warnings() {
+            super(Resource.RESOURCE__WARNINGS, Diagnostic.class);
+        }
 
-		@SuppressWarnings("unchecked")
-		@Override
-		protected void doSetList(Object source, List list, ListDiff diff) {
-			final Resource resource = (Resource) source;
-			resource.getWarnings().clear();
-			resource.getWarnings().addAll(list);
-		}
+        @Override
+        protected void doSetList(Object source, List list, ListDiff diff) {
+            final Resource resource = (Resource) source;
+            resource.getWarnings().clear();
+            resource.getWarnings().addAll(list);
+        }
 
-		@Override
-		protected List doGetList(Object source) {
-			final Resource resource = (Resource) source;
-			return resource.getWarnings();
-		}
-	}
+        @Override
+        protected List doGetList(Object source) {
+            final Resource resource = (Resource) source;
+            return resource.getWarnings();
+        }
+    }
 
-	private static class Modified extends ResourceValueProperty {
-		public Modified() {
-			super(Resource.RESOURCE__IS_MODIFIED, Boolean.class);
-		}
+    private static class Modified extends ResourceValueProperty {
+        public Modified() {
+            super(Resource.RESOURCE__IS_MODIFIED, Boolean.class);
+        }
 
-		@Override
-		protected void doSetValue(Object source, Object value) {
-			final Resource resource = (Resource) source;
-			resource.setModified((Boolean) value);
-		}
+        @Override
+        protected void doSetValue(Object source, Object value) {
+            final Resource resource = (Resource) source;
+            resource.setModified((Boolean) value);
+        }
 
-		@Override
-		protected Object doGetValue(Object source) {
-			final Resource resource = (Resource) source;
-			return resource.isModified();
-		}
-	}
+        @Override
+        protected Object doGetValue(Object source) {
+            final Resource resource = (Resource) source;
+            return resource.isModified();
+        }
+    }
 
-	private static class Loaded extends ResourceValueProperty {
-		public Loaded() {
-			super(Resource.RESOURCE__IS_LOADED, Boolean.class);
-		}
+    private static class Loaded extends ResourceValueProperty {
+        public Loaded() {
+            super(Resource.RESOURCE__IS_LOADED, Boolean.class);
+        }
 
-		@Override
-		protected void doSetValue(Object source, Object value) {
-			final Resource resource = (Resource) source;
-			final boolean load = (Boolean) value;
-			if (load && !resource.isLoaded()) {
-				try {
-					resource.load(resource.getResourceSet().getLoadOptions());
-				} catch (final IOException e) {
-					throw new RuntimeException(e);
-				}
-			} else if (!load && resource.isLoaded()) {
-				resource.unload();
-			}
-		}
+        @Override
+        protected void doSetValue(Object source, Object value) {
+            final Resource resource = (Resource) source;
+            final boolean load = (Boolean) value;
+            if (load && !resource.isLoaded()) {
+                try {
+                    resource.load(resource.getResourceSet().getLoadOptions());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            } else if (!load && resource.isLoaded()) {
+                resource.unload();
+            }
+        }
 
-		@Override
-		protected Object doGetValue(Object source) {
-			final Resource resource = (Resource) source;
-			return resource.isLoaded();
-		}
-	}
+        @Override
+        protected Object doGetValue(Object source) {
+            final Resource resource = (Resource) source;
+            return resource.isLoaded();
+        }
+    }
 
-	private static class ResSet extends ResourceValueProperty {
-		public ResSet() {
-			super(Resource.RESOURCE__RESOURCE_SET, ResourceSet.class);
-		}
+    private static class ResSet extends ResourceValueProperty {
+        public ResSet() {
+            super(Resource.RESOURCE__RESOURCE_SET, ResourceSet.class);
+        }
 
-		@Override
-		protected Object doGetValue(Object source) {
-			return ((Resource) source).getResourceSet();
-		}
+        @Override
+        protected Object doGetValue(Object source) {
+            return ((Resource) source).getResourceSet();
+        }
 
-		@Override
-		protected void doSetValue(Object source, Object value) {
-			final Resource resource = (Resource) source;
-			if (value != resource.getResourceSet()) {
-				final ResourceSet newSet = (ResourceSet) value;
-				resource.getResourceSet().getResources().remove(resource);
-				if (newSet != null) {
-					newSet.getResources().add(resource);
-				}
-			}
-		}
-	}
+        @Override
+        protected void doSetValue(Object source, Object value) {
+            final Resource resource = (Resource) source;
+            if (value != resource.getResourceSet()) {
+                final ResourceSet newSet = (ResourceSet) value;
+                resource.getResourceSet().getResources().remove(resource);
+                if (newSet != null) {
+                    newSet.getResources().add(resource);
+                }
+            }
+        }
+    }
 
-	private static class Contents extends NotifierListProperty {
-		public Contents() {
-			super(Resource.RESOURCE__CONTENTS, EObject.class);
-		}
+    private static class Contents extends NotifierListProperty {
+        public Contents() {
+            super(Resource.RESOURCE__CONTENTS, EObject.class);
+        }
 
-		@SuppressWarnings("unchecked")
-		@Override
-		protected void doSetList(Object source, List list, ListDiff diff) {
-			final Resource resource = (Resource) source;
-			resource.getContents().clear();
-			resource.getContents().addAll(list);
-		}
+        @Override
+        protected void doSetList(Object source, List list, ListDiff diff) {
+            final Resource resource = (Resource) source;
+            resource.getContents().clear();
+            resource.getContents().addAll(list);
+        }
 
-		@Override
-		protected List doGetList(Object source) {
-			final Resource resource = (Resource) source;
-			return resource.getContents();
-		}
-	}
+        @Override
+        protected List doGetList(Object source) {
+            final Resource resource = (Resource) source;
+            return resource.getContents();
+        }
+    }
 
-	private static final IValueProperty MODIFIED = new Modified();
-	private static final IValueProperty LOADED = new Loaded();
-	private static final IValueProperty RESOURCE_SET = new ResSet();
-	private static final IListProperty CONTENTS = new Contents();
-	private static final IListProperty ERRORS = new Errors();
-	private static final IListProperty WARNINGS = new Warnings();
+    private static final IValueProperty MODIFIED = new Modified();
+    private static final IValueProperty LOADED = new Loaded();
+    private static final IValueProperty RESOURCE_SET = new ResSet();
+    private static final IListProperty CONTENTS = new Contents();
+    private static final IListProperty ERRORS = new Errors();
+    private static final IListProperty WARNINGS = new Warnings();
 
-	public static IValueProperty modified() {
-		return MODIFIED;
-	}
+    public static IValueProperty modified() {
+        return MODIFIED;
+    }
 
-	public static IValueProperty loaded() {
-		return LOADED;
-	}
+    public static IValueProperty loaded() {
+        return LOADED;
+    }
 
-	public static IValueProperty resourceSet() {
-		return RESOURCE_SET;
-	}
+    public static IValueProperty resourceSet() {
+        return RESOURCE_SET;
+    }
 
-	public static IListProperty contents() {
-		return CONTENTS;
-	}
+    public static IListProperty contents() {
+        return CONTENTS;
+    }
 
-	public static IListProperty errors() {
-		return ERRORS;
-	}
+    public static IListProperty errors() {
+        return ERRORS;
+    }
 
-	public static IListProperty warnings() {
-		return WARNINGS;
-	}
+    public static IListProperty warnings() {
+        return WARNINGS;
+    }
 }
