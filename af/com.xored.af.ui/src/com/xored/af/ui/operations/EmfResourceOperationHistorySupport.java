2c475ac8853d4d5bc3ece2247e3d5c0d35df3efa
Igor Zapletnev <igor.zapletnev@gmail.com>
2013-09-09 12:21:50 +0700

    AF: Eclipse Puplic Lisence

--- af/com.xored.af.ui/src/com/xored/af/ui/operations/EmfResourceOperationHistorySupport.java
+++ af/com.xored.af.ui/src/com/xored/af/ui/operations/EmfResourceOperationHistorySupport.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2013 xored software, Inc.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     xored software, Inc. - initial API and Implementation (Igor Zapletnev)
+ *******************************************************************************/
 package com.xored.af.ui.operations;
 
 import java.text.MessageFormat;
--- af/com.xored.af/src/com/xored/af/builds/WorkspaceLifecycleJob.java
+++ af/com.xored.af/src/com/xored/af/builds/WorkspaceLifecycleJob.java
@@ -1,6 +1,14 @@
-/**
- * Copyright (C) 2013, 1C
- */
+/*******************************************************************************
+ * Copyright (c) 2010-2013 xored software, Inc.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     xored software, Inc. - initial API and Implementation (Igor Zapletnev)
+ *******************************************************************************/
 package com.xored.af.builds;
 
 import org.eclipse.core.resources.ResourcesPlugin;
